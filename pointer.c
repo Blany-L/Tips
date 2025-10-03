996435c885d7028fd0aa503e4ea2b63d78cf0311
Blany-L <yang.blank.@outlook.com>
2025-10-03 15:16:28 +0800

    add a print_array function in pointer.c

--- pointer.c
+++ pointer.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
+
+void print_array(double *array, int length)
+{
+	for(int i = 0; i < length; i++)
+	{
+		printf("%f\t", array[i]);
+	}
+	putchar('\n');
+}
+
 int main(void)
 {
 	int count = 100;
