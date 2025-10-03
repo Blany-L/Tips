01414d20f3937a0d0ad03bc3afb0c01516a33b57
Blany-L <yang.blank.@outlook.com>
2025-10-03 14:29:06 +0800

    add a log level in print_message function of helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
 
-void print_message(const char *message)
+void print_message(const char *message, int level)
 {
+	switch(level)
+	{
+		case 0:printf("normal.\n");break;
+		case 1:printf("warning.\n");break;
+		case 2:printf("error.\n");break;
+		default:printf("other.\n");break;
+	}
 	printf(message);
 	return;
 }
